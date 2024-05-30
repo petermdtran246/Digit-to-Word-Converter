3551d081e9f61a0eee93fcb0e0bcb3955c06da9a
petermdtran246 <petermdtran246@gmail.com>
2024-05-30 22:54:59 +0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+def DigitToWord(d):
+    if d == 1:
+        print('one')
+    elif d == 2:
+        print('two')
+    elif d == 3:
+        print('three')
+    elif d == 4:
+        print('four')
+    elif d == 5:
+        print('five')
+    elif d == 6:
+        print('six')
+    elif d == 7:
+        print('seven')
+    elif d == 8:
+        print('eight')
+    elif d == 9:
+        print('nine')
+    elif d == 0:
+        print('zero')
+
+d = int(input('Enter Digit number: '))
+DigitToWord(d)
